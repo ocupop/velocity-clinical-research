8a032f18461b9a8f9755e92a66e2e30c9c7ec058
Jon Burr <jonathanwburr@gmail.com>
2020-09-25 12:52:45 -0700

    locations templates and structure, baseline styles, single specialty template,
    test owl carousel, test map, single post layout, news template,

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,74 @@
 $(document).ready(function() {
   console.log('hellow')
 
-  var map = new Datamap({
-    element: document.getElementById('us-map'),
-    scope: 'usa',
-    fills: {
-      defaultFill: '#36c0f8', // Any hex, color name or rgb/rgba value,
-      'locationPin': '#9467bd',
-    },
-    geographyConfig: {
-      highlightOnHover: false,
-      popupOnHover: false
+  if($('#us-map').length) {
+    //d3 map prototype example
+    var map = new Datamap({
+      element: document.getElementById('us-map'),
+      scope: 'usa',
+      fills: {
+        defaultFill: '#36c0f8', // Any hex, color name or rgb/rgba value,
+        'locationPin': '#9467bd',
+      },
+      geographyConfig: {
+        highlightOnHover: false,
+        popupOnHover: false
+      }
+    });
+    var locations = [{
+      name: 'Seattle',
+      radius: 15,
+      city: 'Seattle',
+      state: 'WA',
+      fillKey: 'locationPin',
+      website: 'www.velocityclinical.com',
+      latitude: 47.6062,
+      longitude: -122.3321
     }
-  });
-  var locations = [{
-    name: 'Seattle',
-    radius: 15,
-    city: 'Seattle',
-    state: 'WA',
-    fillKey: 'locationPin',
-    website: 'www.velocityclinical.com',
-    latitude: 47.6062,
-    longitude: -122.3321
+    ];
+
+    map.bubbles(locations, {
+      popupTemplate: function (geo, data) { 
+        return ['<div class="hoverinfo">' +  data.name,
+        '<br/>Location: ' +  data.city + data.state,
+        '<br/>Website: ' +  data.website + '',
+        '</div>'].join('');
+      }
+    });
+
+    var locationMarker = d3.selectAll('circle').on('click', function(){
+      console.log('clicked map marker')
+      $('#exampleModal').modal('toggle')
+    });
   }
-  ];
 
-  map.bubbles(locations, {
-    popupTemplate: function (geo, data) { 
-      return ['<div class="hoverinfo">' +  data.name,
-      '<br/>Location: ' +  data.city + data.state,
-      '<br/>Website: ' +  data.website + '',
-      '</div>'].join('');
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: [
+      '<i class="ri-arrow-left-s-line"></i>',
+      '<i class="ri-arrow-right-s-line"></i>'
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      500: {
+        items: 2
+      },
+      666: {
+        items: 3
+      },
+      991: {
+        items: 2
+      },
+      1156: {
+        items: 3
+      },
     }
-  });
-
-  var locationMarker = d3.selectAll('circle').on('click', function(){
-    console.log('clicked map marker')
-    $('#exampleModal').modal('toggle')
-  });
+  })
+  
 })
